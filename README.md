1052c527964a771507f0407def9218d7159f0b8d
Alex Sánchez <alex.sanchez@streamsets.com>
2019-01-30 17:32:39 +0100

    migrate from old repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# face_classification_scripts
+Useful scripts using during the face classification project
---
+++ run_all_faces.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# This script will execute for all the files in the given directory the face classification algorithm
+#
+
+# Verify that the path has been supplied correctly
+path=$1
+if [[ -z $path ]]
+then
+  echo 'Path needs to be supplied'
+  exit 0
+fi
+
+# Create the output directory if it doesn't exists
+echo "Creating: /Users/alexsanchez/src/tfg/results/$path if it doesn't exists"
+mkdir -p /Users/alexsanchez/src/tfg/results/$path
+
+# For each file in the directory, exectue the request for process it and save it in the results directory
+let "i=0"
+for filename in /Users/alexsanchez/src/tfg/test_samples/$path/*; do
+
+  echo "Processing: /Users/alex/src/tfg/test_samples/$path/$filename"
+  curl -v -F image=@$filename http://localhost:8084/classifyImage > /Users/alexsanchez/src/tfg/results/$path/output_$i.png
+  let "i++"
+done
