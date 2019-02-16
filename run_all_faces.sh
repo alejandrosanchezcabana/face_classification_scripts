23bde6370204e1141f4e15c3c1e4091925e95d4a
Alex Sánchez <alex.sanchez@streamsets.com>
2019-02-16 18:21:45 +0100

    Add new script to add python execution support

--- run_all_faces.sh
+++ run_all_faces.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-# This script will execute for all the files in the given directory the face classification algorithm
+# This script will execute for all the files in the given directory the face classification algorithm.
+# This version is intended to run using the python script directly.
 #
 
 # Verify that the path has been supplied correctly
@@ -16,11 +17,15 @@ fi
 echo "Creating: /Users/alexsanchez/src/tfg/results/$path if it doesn't exists"
 mkdir -p /Users/alexsanchez/src/tfg/results/$path
 
+# Since the python script uses some relative paths to load the datasets and the models we need to be placed on the src directory.
+cd /Users/alexsanchez/src/tfg/libraries/face_classification/src
+
 # For each file in the directory, exectue the request for process it and save it in the results directory
 let "i=0"
 for filename in /Users/alexsanchez/src/tfg/test_samples/$path/*; do
 
-  echo "Processing: /Users/alex/src/tfg/test_samples/$path/$filename"
-  curl -v -F image=@$filename http://localhost:8084/classifyImage > /Users/alexsanchez/src/tfg/results/$path/output_$i.png
+  echo "Processing: $filename"
+  python3 image_emotion_gender_demo.py $filename
+  mv /Users/alexsanchez/src/tfg/libraries/face_classification/images/predicted_test_image.png /Users/alexsanchez/src/tfg/results/$path/output_$i.png
   let "i++"
 done
